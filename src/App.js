d70d71520a3ecbb907bd55322d87dcd87a6edd6b
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-08-24 17:52:51 +1000

    Upload fontend files

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddTutorial from "./components/add-tutorial.component";
+import group5 from "./components/group5";
+import Home from "./components/Home";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-primary">
+          <Link to={"/tutorials"} className="navbar-brand">
+            Generation Au
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/group5"} className="nav-link">
+                group5
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route exact path="/home" component={Home} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+            <Route exact path="/group5" component={group5} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/group5.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class AddTutorial extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test0: null,
+      test1: "",
+      test2: false
+    };
+  }
+
+  render() {
+    return (
+
+        <h1>Hi Team!</h1>
+
+    );
+  }
+}
