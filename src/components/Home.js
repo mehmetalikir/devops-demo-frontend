5b23a11459e229d504db107882a4aec101f31166
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-05 06:17:23 +1000

    Update yml file

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ export default class AddTutorial extends Component {
     render() {
         return (
 
-            <h1>Welcome Home Page  4</h1>
+            <h1>Welcome Home Page  5</h1>
 
         );
     }
