e3c3909a66b401c02c7cc2f753a0a746288fb26d
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-05 17:29:57 +1000

    Update App.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 export default class AddTutorial extends Component {
     constructor(props) {
@@ -17,11 +18,14 @@ export default class AddTutorial extends Component {
             <li className="test">
               <h1> Welcome to Home Page 02 </h1>
                 <h2> This is That </h2>
+
                     <h3> That is This </h3>
                 <div>
                     <img src="https://res.cloudinary.com/generation/image/upload/w_1150,q_auto,f_auto/australia/2019/04/25k-Announcement-1556x874-tiny.jpg" alt="Generation Australia" width="600" height="200"/>
                 </div>
+
             </li>
+
 
         );
     }
