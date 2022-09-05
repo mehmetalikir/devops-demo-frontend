9748074978a2762f809ff62e480d33e0ca147e51
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-05 13:19:50 +1000

    Update App.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ export default class AddTutorial extends Component {
                 <h2> This is That </h2>
                     <h3> That is This </h3>
                 <div>
-                    <img src="https://res.cloudinary.com/generation/image/upload/w_1150,q_auto,f_auto/australia/2019/04/25k-Announcement-1556x874-tiny.jpg" alt="Generation Australia"/>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_1150,q_auto,f_auto/australia/2019/04/25k-Announcement-1556x874-tiny.jpg" alt="Generation Australia" width="600" height="800"/>
                 </div>
             </li>
 
