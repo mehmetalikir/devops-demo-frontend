f1e0590a492b48bf5399ae6803e9e96bae84845e
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-05 13:11:32 +1000

    Update App.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,14 @@ export default class AddTutorial extends Component {
     render() {
         return (
 
-            <h1>Welcome to Home Page 02</h1>
-
+            <li className="test">
+              <h1> Welcome to Home Page 01 </h1>
+                <h2> This is That </h2>
+                    <h3> That is This </h3>
+                <div>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_1150,q_auto,f_auto/australia/2019/04/25k-Announcement-1556x874-tiny.jpg" alt="Generation Australia"/>
+                </div>
+            </li>
 
         );
     }
