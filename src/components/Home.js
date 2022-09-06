3fca12d1b40364c8f53533c39f48a8d444ae79fa
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-06 16:10:42 +1000

    Add picture sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,16 +20,21 @@ export default class AddTutorial extends Component {
                 <h6> This is That </h6>
                     <h10> That is This </h10>
                 <div>
+
                     <picture>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" alt="Example image"/>
+
+                    <img style="float: left;" src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" alt="Example image"/>
+                    <img style="float: left;" src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-boy-black-gown-holding-diploma_43633-12257.jpg?w=740" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume_353337-1077.jpg" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume-holding-diploma_353337-1075.jpg" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume-holding-diploma_353337-1076.jpg" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-boy-graduation-celebration-white-background_353337-1078.jpg" alt="Example image"/>
+
                     </picture>
-                    </div>
+
+                </div>
+
             </li>
 
 
