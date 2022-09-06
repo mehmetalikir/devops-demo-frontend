db829c0421639f358bc70130989eae95bb67aed4
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-06 16:52:47 +1000

    Update img-wrapper Class

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,14 +17,9 @@ export default class AddTutorial extends Component {
     render() {
         return (
 
-            <div className="Img">
-                    <img src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-boy-black-gown-holding-diploma_43633-12257.jpg?w=740" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume_353337-1077.jpg" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume-holding-diploma_353337-1075.jpg" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume-holding-diploma_353337-1076.jpg" alt="Example image"/>
-                    <img src="https://img.freepik.com/premium-vector/cartoon-boy-graduation-celebration-white-background_353337-1078.jpg" alt="Example image"/>
+            <div id="img-wrapper">
+                <div><img src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" /></div>
+                <div><img src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" /></div>
             </div>
 
         );
