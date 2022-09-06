72d4e8b10929d53371dbd5b4ca9a845902ddae0b
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-09-06 16:43:47 +1000

    Update Img Class

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,8 +18,8 @@ export default class AddTutorial extends Component {
         return (
 
             <div className="Img">
-                    <img style="float: left;" src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" alt="Example image"/>
-                    <img style="float: left;" src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" alt="Example image"/>
+                    <img src="https://img.freepik.com/premium-vector/cartoon-little-boy-graduation-costume-holding-diploma_353337-1074.jpg?w=740" alt="Example image"/>
+                    <img src="https://img.freepik.com/premium-vector/beautiful-girl-graduation-day-with-diploma-illustration_43633-12251.jpg?w=740" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-boy-black-gown-holding-diploma_43633-12257.jpg?w=740" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume_353337-1077.jpg" alt="Example image"/>
                     <img src="https://img.freepik.com/premium-vector/cartoon-little-girl-graduation-costume-holding-diploma_353337-1075.jpg" alt="Example image"/>
